a9e1be96c03f019a7c908c1f9d8550e3c9ee77e5
Jennifer Cheung <csjcheung@connect.ust.hk>
2025-02-26 00:16:37 +0800

    feat: Can deal cards to the player.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useCallback, useEffect, useState } from 'react'
 import { initialCards, Suit, type Card as CardType } from './utils'
-import './App.css'
+import styles from './App.module.scss'
 import Card from './components/Card/Card'
 import { random } from 'lodash'
 
 function App() {
 	const [pool, setPool] = useState<CardType[]>(initialCards)
 	const [deckCard, setDeckCard] = useState<CardType>()
+	const [myHand, setMyHand] = useState<CardType[]>()
 
 	const revealDeck = useCallback(() => {
 		const randomIndex = random(0, pool.length - 1)
@@ -22,21 +23,51 @@ function App() {
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
+	const deal = useCallback(() => {
+		const myIndices: number[] = []
+		for (let i = 0; i < 13; i++) {
+			let randomIndex = random(0, 51)
+			while (myIndices.find((i) => {
+				return i === randomIndex
+			})) {
+				randomIndex = random(0, 51)
+			}
+			myIndices.push(randomIndex)
+		}
+		const newPool = [...pool]
+		const myCards = myIndices.map((index) => {
+			newPool.toSpliced(index, 1)
+			return pool[index]
+		})
+		setMyHand(myCards)
+	}, [pool])
+
 	return (
-		<div>
+		<div className={styles.container}>
 			{/* Deck */}
 			<Card suit={deckCard ? deckCard.suit : Suit.Diamonds} rank={deckCard ? deckCard.rank : 7} />
 
 			{/* Opponent's hand */}
+			<div className={styles.hand}>
+				{[...Array(13)].map((value, i) => (
+					<Card flipped key={i} />
+				))}
+			</div>
 
-			{/* Playing cards */}
+			{/* Arena */}
+			<div className={styles.arena}></div>
 
 			{/* My hand */}
+			<div className={styles.hand}>
+				{myHand?.map((card, i) => (
+					<Card suit={card.suit} rank={card.rank} key={i} />
+				))}
+			</div>
 
 			{/* Message */}
 
 			{/* Action button */}
-			<button onClick={revealDeck}>NEXT STEP</button>
+			<button onClick={deal}>NEXT STEP</button>
 		</div>
 	)
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,17 @@ import { getUnicodeCard, Suit } from "../../utils"
 import styles from './Card.module.scss'
 
 type CardProps = {
-    suit: Suit,
-    rank: number | string
+    suit?: Suit,
+    rank?: number | string,
+    flipped?: boolean
 }
 
-const Card = ({ suit, rank }: CardProps) => {
-    return <span className={styles.card}>{getUnicodeCard(suit, rank)}</span>
+const Card = ({ suit, rank, flipped=false }: CardProps) => {
+    if (flipped) {
+        return (<span className={styles.card}>{String.fromCodePoint(127136)}</span>)
+    } else {
+        return (<span className={styles.card}>{getUnicodeCard(suit!, rank!)}</span>)
+    }
 }
 
 export default Card
